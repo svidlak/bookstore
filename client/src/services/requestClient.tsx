e0dc0e017546b2a962e0cb786b39be69c466c3db
svidlak <svid320@gmail.com>
2024-01-10 22:51:48 +0200

    added env variables for api basepath

--- client/src/services/requestClient.tsx
+++ client/src/services/requestClient.tsx
@@ -1,7 +1,10 @@
 import axios from 'axios'
+const { MODE, DEV_BASE_PATH, PROD_BASE_PATH } = import.meta.env
 
-const API_BASE_PATH = 'http://localhost:3000/api/v1'
-export const IMAGE_BASE_PATH = 'http://localhost:3000'
+const BASE_PATH = MODE === "development" ? DEV_BASE_PATH : PROD_BASE_PATH;
+
+const API_BASE_PATH = `${BASE_PATH}/api/v1`
+export const IMAGE_BASE_PATH = BASE_PATH
 
 export const client = axios.create({
     baseURL: API_BASE_PATH,
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.DEV_BASE_PATH': JSON.stringify('http://localhost:3000'),
+    'import.meta.env.PROD_BASE_PATH': JSON.stringify('http://18.201.37.94:3000')
+  }
 })
